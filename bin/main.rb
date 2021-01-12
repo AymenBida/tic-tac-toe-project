f403f03d9405164d40c94252c668f71a1ab9ee03
patrick <angelosp07@gmail.com>
2021-01-12 15:24:01 +0200

    Add instructions

--- bin/main.rb
+++ bin/main.rb
@@ -2,15 +2,37 @@
 
 require_relative '../lib/game_logic.rb'
 
+def instructions
+  system('clear')
+  puts "You will have a 3x3 board and player 1 will have the first turn\n"
+  puts "When it's your turn enter the number of the square that you want to play (between 1 and 9)\n\n"
+  puts '    -------------'
+  puts '    | 1 | 2 | 3 |'
+  puts '    -------------'
+  puts '    | 4 | 5 | 6 |'
+  puts '    -------------'
+  puts '    | 7 | 8 | 9 |'
+  puts '    -------------'
+  puts "\nP.S: Don't choose squares that are already occupied"
+  puts "The winner is the first one to form a line with 3 symbols vertically, horizontally or diagonally\n\n"
+  puts 'Press Enter to continue'
+  gets.chomp
+end
+
 puts 'Welcome to Tic-Tac-Toe by Aymen and Patrick'
+
 puts 'Player 1, please enter your name:'
 player1 = Player.new(gets.chomp, 'X')
 puts "Hello, #{player1.name}, you are '#{player1.letter}'\n\n"
+
 puts 'Player 2, please enter your name:'
 player2 = Player.new(gets.chomp, 'O')
 puts "Hello, #{player2.name}, you are '#{player2.letter}'\n\n"
+
 puts "Enter 'y' if you want to see the instructions (Press enter to continue)"
-puts 'Instructions\n\n' if gets.chomp.downcase == 'y'
+instructions if gets.chomp.downcase == 'y'
+system('clear')
+
 b = Board.new
 puts 'Creating your board...'
 sleep(1.2)
@@ -29,6 +51,7 @@ loop do
   elsif b.draw?
     break
   end
+
   puts "\n#{player2.name}, it's your move\n"
   puts 'invalid move! please enter a number between 1 and 9' until b.make_move?(gets.chomp.to_i, player2)
   system('clear')
@@ -41,6 +64,7 @@ loop do
     break
   end
 end
+
 puts winner ? "\nCongratulations #{winner}!" : "It's a draw, nobody won!"
 puts "Thank you for playing !\n\n\n\n"
 puts 'Sleeping...'
