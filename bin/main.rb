a17ec444a564be5f5ed7a0a9284fc8ac351c3549
patrick <angelosp07@gmail.com>
2021-01-12 12:52:25 +0200

    Update branch

--- bin/main.rb
+++ bin/main.rb
@@ -10,36 +10,50 @@ answer = gets.chomp.downcase
 puts 'Instructions' if answer == 'y'
 loop do
   winner = false
-  board.display # This displays the board state
+  3.times do |_i|
+    puts '-------------'
+    puts '|   |   |   |'
+  end
+  puts '-------------'
   loop do
     # Player 1 turn
     puts "#{name1} it's your move"
-    until move.is_valid?
-      move = gets.chomp # This will get the players input
-      puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
-    end
+    # until the move is valid?
+    move = gets.chomp # This will get the players input
+    # puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
     puts "#{name1}, your move was : #{move}"
-    board.display # This displays the board state
-    if move.win?
-      winner = name1
-      break
-    elsif draw == true
-      break
-    end
+    puts '-------------'
+    puts '|   |   |   |'
+    puts '-------------'
+    puts '|   | X |   |'
+    puts '-------------'
+    puts '|   |   |   |'
+    puts '-------------'
+    # if move.win?
+    # winner = name1
+    # break
+    # elsif draw == true
+    # break
+    # end
     # Player 2 turn
     puts "#{name2} it's your move"
-    until move.is_valid?
-      move = gets.chomp # This will get the players input
-      puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
-    end
+    # until move.is_valid?
+    move = gets.chomp # This will get the players input
+    # puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
     puts "#{name2}, your move was : #{move}"
-    board.display # This displays the board state
-    if move.win?
-      winner = name2
-      break
-    elsif draw == true
-      break
-    end
+    puts '-------------'
+    puts '|   | O |   |'
+    puts '-------------'
+    puts '|   | X |   |'
+    puts '-------------'
+    puts '|   |   |   |'
+    puts '-------------'
+    # if move.win?
+    # winner = name2
+    # break
+    # elsif draw == true
+    # break
+    # end
   end
   puts winner ? "Congratulations #{winner}!" : "It's a draw, nobody won!"
   puts 'Do you want to rematch ?'
@@ -47,5 +61,5 @@ loop do
   answer = gets.chomp.downcase
   break unless answer == 'y'
 end
-exit(true)
 # rubocop:enable Metrics/BlockLength
+exit(true)
