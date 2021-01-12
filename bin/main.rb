d4a333eb913930ffbe8e2f8bb10a2e59c6e7ec88
AymenBida <aymen.bida.00@gmail.com>
2021-01-12 10:34:15 +0100

    Fix the crashing issue

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# rubocop:disable Metrics/BlockLength
 puts 'Welcome to Tic-Tac-Toe by Aymen and Patrick'
 puts 'Player 1, please enter your name: '
 name1 = gets.chomp
@@ -10,36 +9,34 @@ answer = gets.chomp.downcase
 puts 'Instructions' if answer == 'y'
 loop do
   winner = false
-  board.display # This displays the board state
+  puts 'Displays board...' # board.display # This displays the board state
   loop do
     # Player 1 turn
     puts "#{name1} it's your move"
-    until move.is_valid?
-      move = gets.chomp # This will get the players input
-      puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
-    end
+    # until the move is valid?
+    move = gets.chomp # This will get the players input
+    # puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
     puts "#{name1}, your move was : #{move}"
-    board.display # This displays the board state
-    if move.win?
-      winner = name1
-      break
-    elsif draw == true
-      break
-    end
+    puts 'Displays board...' # board.display # This displays the board state
+    # if move.win?
+    # winner = name1
+    # break
+    # elsif draw == true
+    # break
+    # end
     # Player 2 turn
     puts "#{name2} it's your move"
-    until move.is_valid?
-      move = gets.chomp # This will get the players input
-      puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
-    end
+    # until move.is_valid?
+    move = gets.chomp # This will get the players input
+    # puts "Your move (#{move}) is not valid, try another move" unless move.is_valid?
     puts "#{name2}, your move was : #{move}"
-    board.display # This displays the board state
-    if move.win?
-      winner = name2
-      break
-    elsif draw == true
-      break
-    end
+    puts 'Displays board...' # board.display # This displays the board state
+    # if move.win?
+    # winner = name2
+    # break
+    # elsif draw == true
+    # break
+    # end
   end
   puts winner ? "Congratulations #{winner}!" : "It's a draw, nobody won!"
   puts 'Do you want to rematch ?'
@@ -48,4 +45,3 @@ loop do
   break unless answer == 'y'
 end
 exit(true)
-# rubocop:enable Metrics/BlockLength
