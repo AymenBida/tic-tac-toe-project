7cba7fd90cb3bac84aa4fa84307cc32eafa850b8
AymenBida <aymen.bida.00@gmail.com>
2021-01-12 11:14:22 +0100

    Add draw? and win? methods to the Board class

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,6 +1,7 @@
 class Board
   attr_reader :moves
-  
+  WIN_CASES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
   def initialize
     @board = Array.new(9, ' ')
     @moves = 0
@@ -23,5 +24,18 @@ class Board
     false
   end
 
-  def 
+  def draw?
+    true if @moves == 9
+  end
+
+  def win?
+    if @moves > 4
+      WIN_CASES.each do |a_case|
+        return true if @board[a_case[0]] != ' ' && @board[a_case[0]] == @board[a_case[1]] && @board[a_case[0]] == @board[a_case[2]]
+      end
+      false
+    else
+      false
+    end
+  end
 end
